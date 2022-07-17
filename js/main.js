b5ce91a1cc6afc0a6b6131e38fcb187fd85c9f98
Alexey472 <101046394+Alexey472@users.noreply.github.com>
2022-07-17 19:15:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ $(function () {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
@@ -37,9 +37,6 @@ $(function () {
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   })
 
