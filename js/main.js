201dc837ed5caba567428c6ac9cdfd99d7cdefa3
Alexey472 <101046394+Alexey472@users.noreply.github.com>
2022-07-17 13:35:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,8 +12,46 @@ $(function () {
     infinite: true,
     dots: true,
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   })
 
 
 });
+
+$(function () {
+  
+  $('.header__burger').click(function(event) {
+    $('.header__burger, .header__inner-nav').toggleClass('open');
+  })
+
+
+});
+
